b7c5b807044a99d6ddd7ee736003654b71959ee2
kr-sharath <krsharath16@gmail.com>
2024-12-19 11:34:52 +0530

    Code change to remove comments

--- s3GetObject.sh
+++ s3GetObject.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-#Output=output.txt
-#aws s3api list-objects --bucket sample-devops-bucket --query "Contents[*].Key" --output table > $Output
-#echo "File save to, $Output"
 
 OBJECT_KEYS=$(aws s3api list-objects --bucket sample-devops-bucket)
 
 # Write the output to a .txt file
 echo "$OBJECT_KEYS" > s3_objects.txt
 echo "$OBJECT_KEYS"
-# Optional: Print a confirmation message
 echo "The S3 object keys have been saved to s3_objects.txt."
